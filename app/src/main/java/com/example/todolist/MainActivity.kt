15916e45814d8a8af7b88e43ded4db5b74d8ac8f
dowoniscool <dowoniscool@yahoo.com>
2020-11-24 06:53:06 +0100

    Bootcamp Day 1 Connect
    MainActivity.kt and AddItemActivity.kt
    
    activity_main.xml and activity_add_item.xml

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.todolist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoItemRecyclerView : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        todoItemRecyclerView = findViewById(R.id.todo_item_recycler_view)
+
         //Test commit from iMac
+    }
+
+    public fun navToAddItemAction(view: View) {
+
+        val intent: Intent = Intent(this, AddItemActivity::class.java)
+        startActivity(intent)
+
     }
     //Tasks to accomplish:
     //Build the main activity UI
